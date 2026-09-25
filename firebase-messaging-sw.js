5c3d8bd28ec585fcd72e6fab918820496a694aab
kloetje1969-droid <kloetje1969@gmail.com>
2026-09-25 07:58:33 +0200

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyC2TVkuO8vSfzmaGu-CictLit5iuRnRFNU",
+    authDomain: "padel-app-b8362.firebaseapp.com",
+    databaseURL: "https://padel-app-b8362-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "padel-app-b8362",
+    storageBucket: "padel-app-b8362.appspot.com",
+    messagingSenderId: "453639626765",
+    appId: "1:453639626765:web:f71b5164fca8c3bebef544",
+    measurementId: "G-SN9VJ6Y4YF"
+});
+
+const messaging = firebase.messaging();
